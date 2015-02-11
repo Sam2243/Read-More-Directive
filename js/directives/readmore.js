f2597ce3b0ff183b16fe17f2ba8e5a35289842c7
Sameer Verasiya <ssverasiya@etilizepak.com>
2015-02-11 17:51:55 +0500

    - Added ng-bind-html attribute to the text in the template variable of the directive to cater texts having html

--- js/directives/readmore.js
+++ js/directives/readmore.js
@@ -18,7 +18,7 @@ readMore.directive('readMore', function() {
         scope: {
             text: '=ngModel'
         },
-        template:  "<p> {{text | readMoreFilter:[text, countingWords, textLength] }}" +
+        template:  "<p><span ng-bind-html='text | readMoreFilter:[text, countingWords, textLength]'></span>" +
             "<a ng-show='showLinks' ng-click='changeLength()' class='color3'>" +
             "<strong ng-show='isExpanded'>  Show Less</strong>" +
             "<strong ng-show='!isExpanded'>  Show More</strong>" +
